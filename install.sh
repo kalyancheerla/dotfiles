5d4cf7a9ea9e10d9735b946995784cbea72bff6e
kalyan <32354220+kalyancheerla@users.noreply.github.com>
2021-06-26 23:51:52 +0530

    adding vim config & setup

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ declare -a DOTFILES=(
 ".extras"
 ".inputrc"
 ".tmux.conf"
+".vimrc"
 ".gdbinit")
 
 echo "Sourcing dotfiles started..."
@@ -29,3 +30,14 @@ for file in "${DOTFILES[@]}"; do
         ln -s "$DOTFILESPATH/$file" "$HOME/$file"
     fi
 done
+
+echo "Setting up vim directory..."
+if [ -d "$HOME/.vim" ]; then
+    echo "[+] moving $HOME/.vim -> $HOME/.vim.orig"
+    rm -rf "$HOME/.vim.orig"
+    mv "$HOME/.vim" "$HOME/.vim.orig"
+fi
+echo "[+] copying $DOTFILESPATH/.vim -> $HOME/.vim"
+cp -r "$DOTFILESPATH/.vim" "$HOME/.vim"
+echo "[+] removing gitkeep files"
+find "$HOME/.vim" -name '.gitkeep' -size 0c -delete
